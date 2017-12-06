600321bbcc3566c5715e01592174a6c78f611d65
scalka <calka.sylwia@gmail.com>
2017-12-06 15:17:21 +0000

    mongo node express start files

---
+++ public/client.js
@@ -0,0 +1,7 @@
+//Client side code
+console.log('Client-side code running');
+
+const button = document.getElementById('myButton');
+button.addEventListener('click', function(e) {
+  console.log('button was clicked');
+});
