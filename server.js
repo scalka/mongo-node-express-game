d7d8d1337a2deb886755ba77569533eac6f1f001
scalka <calka.sylwia@gmail.com>
2017-12-06 15:47:40 +0000

    working

--- server.js
+++ server.js
@@ -1,19 +1,55 @@
-//Server side code
 console.log('Server-side code running');
 
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
 
 // serve files from the public directory
 app.use(express.static('public'));
 
-// start the express web server listening on 8080
-app.listen(8080, () => {
-  console.log('listening on 8080');
+// connect to the db and start the express server
+let db;
+
+// Replace the URL below with the URL for your database
+//const url =  'mongodb://user:password@mongo_address:mongo_port/clicks';
+const url = 'mongodb://localhost:27017/clicks';
+
+MongoClient.connect(url, (err, database) => {
+  if(err) {
+    return console.log(err);
+  }
+  db = database;
+  // start the express web server listening on 8080
+  app.listen(8080, () => {
+    console.log('listening on 8080');
+  });
 });
 
 // serve the homepage
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+// add a document to the DB collection recording the click event
+app.post('/clicked', (req, res) => {
+  const click = {clickTime: new Date()};
+  console.log(click);
+  console.log(db);
+
+  db.collection('clicks').save(click, (err, result) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('click added to db');
+    res.redirect('/');
+  });
+});
+
+// get the click data from the database
+app.get('/clicks', (req, res) => {
+  db.collection('clicks').find().toArray((err, result) => {
+    if (err) return console.log(err);
+    res.send(result);
+  });
 });
