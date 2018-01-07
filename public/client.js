ce1e06c0a5c787acbfff05df5a429b7227a85bb0
scalka <calka.sylwia@gmail.com>
2018-01-07 12:35:33 +0000

    // TODO finish logic for touching

--- public/client.js
+++ public/client.js
@@ -3,7 +3,7 @@
 // Circle Parent class (or subclass) - Player class
 // ----------------------------
 class Circle {
-  constructor(x = 50, y = 50, r = 10, col = '#ffc689') {
+  constructor(x = 0, y = 0, r = 10, col = '#ffc689') {
     this.x = x;
     this.y = y;
     this.r = r;
@@ -39,29 +39,31 @@ class Circle {
 class TailedCircle extends Circle {
   constructor(x = 50, y = 50, r = 10, col = '#ffc689') {
     super(x, y, r, col);
-    this.diff = 10;
     this.num_segments = 1;
     this.x_start = this.x;
     this.y_start = this.y;
     this.x_cor = [];
     this.y_cor = [];
-    this.tail = this.num_segments * rect(81, 81, 63, 63);
+    this.line_x = [];
+    this.line_y = [];
+    this.gridTouch = false;
   }
   draw() {
     fill(this.col);
     ellipse(this.x, this.y, this.r);
     return this; // allows method chaining
   }
-  drawTail() {
+  drawTail(grid) {
     noFill();
     stroke(255, 0, 0);
     beginShape();
-    for (let i = 0; i < this.num_segments; i++) {
+    for (let i = 0; i < this.x_cor.length; i++) {
       // Check for this.x_cor[i + 1] so no empty variable in spot #2 (zero-based index)
       if (this.x_cor[i + 1]) {
+        this.line_x[i] = Math.round(this.x_cor[i + 1]);
+        this.line_y[i] = Math.round(this.y_cor[i + 1]);
         line(this.x_cor[i], this.y_cor[i], this.x_cor[i + 1], this.y_cor[i + 1]);
-        // vertex(this.x_cor[i], this.y_cor[i]);
-        // vertex(this.x_cor[i + 1], this.y_cor[i + 1]);
+
       }
       // endShape();
       fill(255, 204, 0, 50);
@@ -70,16 +72,36 @@ class TailedCircle extends Circle {
     this.y_cor.push(this.y);
     return this;
   }
-  move() {
+  move(grid) {
     // reverse direction ('bounce')
-    this.collision().drawTail();
-    this.num_segments += 1;
+    //console.log(grid);
+    this.expandTerrain(grid, this.x, this.y);
+    this.collision().drawTail(grid);
     this.x += this.vx;
     this.y += this.vy;
 
     return this; // allows method chaining
   }
 
+  expandTerrain(grid, x, y) {
+    //console.log(x);
+    //console.log(this.line_x);
+    if ( this.line_x.includes(Math.round(x)) && this.line_y.includes(Math.round(y)) ) {
+      console.log("x touch");
+      let columnsArray = [];
+      let rowsArray = [];
+
+      this.line_x.forEach(function(x_coord) {
+          columnsArray =Math.round(x_coord/100);
+      });
+      this.line_y.forEach(function(y_coord) {
+        rowsArray = Math.round(y_coord/100);
+      });
+      this.gridTouch = true;
+    } else {
+      this.gridTouch = false;
+    }
+  }
   collision(objectA, objectB) {
     if (this.x <= 0 || this.x >= 1000 ) {
       this.vx = -this.vx;
@@ -99,10 +121,6 @@ class Grid {
     this.rowsCount = rows; // The height of the map, in rows
     this.colsCount = cols; // The width of the map, in columns
     this.tileSize = 64; // The tile size, in pixels
-  }
-  // Gets the cell index in a certain position
-  getCell(col, row) {
-     return this.grid[col][row];
   }
 
   render() {
@@ -124,7 +142,8 @@ class Grid {
 
   gridTouch(circle, column, row) {
     //console.log(`Circle is touching grid`);
-    console.log(this.getCell(4,15));
+    //console.log(circle);
+
     stroke(100);
     this.cell = {
       column: column,
@@ -150,7 +169,7 @@ socket.on('news', function (data) {
 //PROCESSING at start
 function setup() {
 	createCanvas(1000, 1000);
-  player = new TailedCircle(500, 500, 50);
+  player = new TailedCircle(50, 50, 50);
   grid = new Grid(30, 30);
   grid.render();
 }
@@ -159,8 +178,10 @@ function draw() {
 	//clear background
 	//background("#2834ff");
   grid.render();
-  player.draw().move();
-  grid.gridTouch(player, 4, 5);
+  player.draw().move(grid);
+  if (player.gridTouch){
+    grid.gridTouch(player, 4, 5);
+  }
 }
 //PROCESSING every mouseclick
 function mousePressed() {
